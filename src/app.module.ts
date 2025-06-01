b36d5a238b97195d791d3e89e10dfe469c7181ee
peterxyjz <huyq09@gmail.com>
2025-06-02 02:11:00 +0700

    feat: cronjob

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import CustomZodValidationPipe from '@/common/pipes/custom-zod-validation.pipe'
+import { RemoveRefreshTokenCronjob } from '@/cronjobs/remove-refresh-token.cronjob'
 import { PermissionModule } from '@/routes/permission/permission.module'
 import { RoleModule } from '@/routes/role/role.module'
 import { HttpExceptionFilter } from '@/shared/filters/http-exception.filter'
 import { Module } from '@nestjs/common'
 import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core'
+import { ScheduleModule } from '@nestjs/schedule'
 import { ZodSerializerInterceptor } from 'nestjs-zod'
 import { AccountModule } from './routes/account/account.module'
 import { AuthModule } from './routes/auth/auth.module'
@@ -15,6 +17,7 @@ import { SharedModule } from './shared/shared.module'
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     SharedModule,
     AuthModule,
     RoleModule,
@@ -35,7 +38,8 @@ import { SharedModule } from './shared/shared.module'
     {
       provide: APP_FILTER,
       useClass: HttpExceptionFilter
-    }
+    },
+    RemoveRefreshTokenCronjob
   ]
 })
 export class AppModule {}
