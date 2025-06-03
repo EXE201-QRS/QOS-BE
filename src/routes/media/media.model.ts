c1282d70d33ef4f09b74d3972b9f49833ee9d21d
phongnqse172516@gmail.com <phongnqse172516@gmail.com>
2025-06-03 14:03:06 +0700

    feat: media upload file with S3 (#30)

---
+++ src/routes/media/media.model.ts
@@ -0,0 +1,29 @@
+import { MEDIA_FOLDER_NAME } from '@/common/constants/other.constant'
+import { z } from 'zod'
+
+export const PresignedUploadFileBodySchema = z
+  .object({
+    extension: z.string()
+  })
+  .strict()
+
+export const UploadFileResSchema = z.object({
+  data: z.array(
+    z.object({
+      url: z.string()
+    })
+  )
+})
+
+export const GetFolderFileQuerySchema = z
+  .object({
+    folderName: z.enum([
+      MEDIA_FOLDER_NAME.AVATAR,
+      MEDIA_FOLDER_NAME.DISH,
+      MEDIA_FOLDER_NAME.IMAGE,
+      MEDIA_FOLDER_NAME.FILE
+    ])
+  })
+  .strict()
+
+export type GetFolderFileQuerySchemaType = z.infer<typeof GetFolderFileQuerySchema>
---
+++ src/shared/services/S3.service.ts
@@ -0,0 +1,60 @@
+import envConfig from '@/config/env.config'
+import { PutObjectCommand, S3 } from '@aws-sdk/client-s3'
+import { Upload } from '@aws-sdk/lib-storage'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { Injectable } from '@nestjs/common'
+import { readFileSync } from 'fs'
+import mime from 'mime-types'
+
+@Injectable()
+export class S3Service {
+  private s3: S3
+  constructor() {
+    this.s3 = new S3({
+      region: envConfig.S3_REGION,
+      credentials: {
+        accessKeyId: envConfig.S3_ACCESS_KEY,
+        secretAccessKey: envConfig.S3_SECRET_KEY
+      }
+    })
+  }
+
+  uploadFile({
+    fileName,
+    filePath,
+    contentType
+  }: {
+    fileName: string
+    filePath: string
+    contentType: string
+  }) {
+    const parallelUploads3 = new Upload({
+      client: this.s3,
+      params: {
+        Bucket: envConfig.S3_BUCKET_NAME,
+        Key: fileName,
+        Body: readFileSync(filePath),
+        ContentType: contentType
+      },
+      queueSize: 4,
+      partSize: 1024 * 1024 * 5,
+      leavePartsOnError: false
+    })
+
+    parallelUploads3.on('httpUploadProgress', (progress) => {
+      console.log(progress)
+    })
+
+    return parallelUploads3.done()
+  }
+
+  createPresignedUrlWithClient = (filename: string) => {
+    const contentType = mime.lookup(filename) || 'application/octet-stream'
+    const command = new PutObjectCommand({
+      Bucket: envConfig.S3_BUCKET_NAME,
+      Key: filename,
+      ContentType: contentType
+    })
+    return getSignedUrl(this.s3, command, { expiresIn: 3600 })
+  }
+}
